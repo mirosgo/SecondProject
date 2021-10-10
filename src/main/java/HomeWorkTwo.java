10f26705610e12d984625171d24ddf0aa368ed21
mirgo <rimlog.94@gmail.com>
2021-10-10 21:22:48 +0300

    вторая домашка(исправления)

--- src/main/java/HomeWorkTwo.java
+++ src/main/java/HomeWorkTwo.java
@@ -1,7 +1,7 @@
 public class HomeWorkTwo {
     public static void main(String[] args){
-       System.out.println(twoNumbers(7,7));
-        Numbers(0);
+       System.out.println(twoNumbers(0,7));
+        numbersOne(0);
         System.out.println(trueFalse(5));
         stringAndNumbers( "hello", 1);
         yearYears(2021);
@@ -13,19 +13,16 @@ public class HomeWorkTwo {
 
     public static boolean twoNumbers(int a, int b){    // сначала не смог проверить работу метода.
         int c = a + b;            // потом в main поместил метод в System.out.println и теперь выводится в консоль
-        if (c>10 && c<=20){
-            return true;
-        }
-        else {
-            return false;
-        }
+        return (c>10 && c<=20);
+
+
     }
 
     //        2. Написать метод, которому в качестве параметра передается целое число,
     //        метод должен напечатать в консоль, положительное ли число передали или отрицательное.
     //        Замечание: ноль считаем положительным числом.
 
-    public  static int Numbers(int a){
+    public  static int numbersOne(int a){
         if (a>=0){
             System.out.println("Положительное число");
             return a;
@@ -40,12 +37,7 @@ public class HomeWorkTwo {
 //        Метод должен вернуть true, если число отрицательное, и вернуть false если положительное.
 
     public static boolean trueFalse(int a){   //не смог проверить работу метода - так же как и с 1 заданием
-        if (a>=0){
-            return false;
-        }
-        else {
-            return true;
-        }
+        return (a<0);
     }
     //        4. Написать метод, которому в качестве аргументов передается строка и число,
     //        метод должен отпечатать в консоль указанную строку, указанное количество раз;
@@ -60,11 +52,18 @@ public class HomeWorkTwo {
     //        кроме каждого 100-го, при этом каждый 400-й – високосный.
 
     public static boolean yearYears(int a){ // не смог довести до ума, чтобы и то и то работало. что то одно только
-        if(a%400==0 || a%4==0){
-         System.out.println("високосный - "+true);
-        } return true;
+       // if(a%400==0 || a%4==0){
+         //System.out.println("високосный - "+true);
+        //} return true;
      //  if(a%100==0 || a>0){
        //     System.out.println("не високосный - "+false);
         //} return false;
+
+        if  ((a%4==0 && a%100!=0) || a%400==0){     //спасибо
+            System.out.println("високосный - "+true);
+        }
+        else   {
+           System.out.println("не високосный - "+false);
+        } return false;
     }
 }
